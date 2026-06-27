d2ca1d73ead8f58b3958a639577eb536137852da
Matheus Mangueira <98111351+MatheusMangueira@users.noreply.github.com>
2026-06-27 19:59:38 +0200

    Update message in server.js for auto deploy

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const html = (count) => `
   </style>
 </head>
 <body>
-  <p>contador salvo no banco de dados</p>
+  <p>contador salvo no banco de dados AUTO DEPLOY</p>
   <h1>${count}</h1>
   <br/>
   <form method="POST" action="/increment">
