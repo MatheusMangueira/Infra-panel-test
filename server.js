fd9769c88e7bfde5a54403ccc65fc2b97dfee6fa
Matheus Mangueira <98111351+MatheusMangueira@users.noreply.github.com>
2026-06-27 18:35:11 +0200

    Update response message in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Hello from InfraPanel!');
+  res.end('Hello from InfraPanel. 0.0.2V auto deploy');
 });
 server.listen(3000, () => console.log('rodando na porta 3000'));
